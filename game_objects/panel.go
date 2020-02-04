8a2e98eb24fbdd1632504d7c42f8960ff5e6926a
y444 <541m0nk1n9@gmail.com>
2020-02-04 18:29:27 +0300

    somwethin s brewin

---
+++ game_objects/panel.go
@@ -0,0 +1,69 @@
+components {
+  id: "block_spawner"
+  component: "/scripts/block_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "block_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/block.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "coin_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/coin.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "small_chest_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/small_chest.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

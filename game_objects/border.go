01f92f850436ef3f982229a756a5323e87ee626e
y444 <541m0nk1n9@gmail.com>
2020-02-07 00:36:20 +0300

    add torches mechanics, start working on borders

--- game_objects/border.go
+++ game_objects/border.go
@@ -1,8 +1,23 @@
+components {
+  id: "border"
+  component: "/scripts/border.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/sprite_atlases/dungeon_items.atlas\"\n"
-  "default_animation: \"torch_unlit\"\n"
+  "default_animation: \"border_left_1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
--- game_objects/panel.go
+++ game_objects/panel.go
@@ -31,3 +31,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "border_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/border.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

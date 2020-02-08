63f5df2ff4ac3ebc1cbbdbdfdf1bc83af2719448
y444 <541m0nk1n9@gmail.com>
2020-02-08 15:36:56 +0300

    implement reward spawning

--- game_objects/block.go
+++ game_objects/block.go
@@ -33,3 +33,39 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "coin_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/coin.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "small_chest_factory"
+  type: "factory"
+  data: "prototype: \"/game_objects/small_chest.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

57c3fc0536899ac4aa62412a1c881b2c8fed3e6d
y444 <541m0nk1n9@gmail.com>
2020-02-11 00:52:22 +0300

    fix trimming bug

--- game_objects/panel.go
+++ game_objects/panel.go
@@ -49,23 +49,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/sprite_atlases/dungeon_items.atlas\"\n"
-  "default_animation: \"bw_skull\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 4.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}

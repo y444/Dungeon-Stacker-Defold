057382a60727da3eb8a3b4b416833d684d7df807
y444 <541m0nk1n9@gmail.com>
2020-02-18 18:15:23 +0300

    working back up

--- modules/world.lua
+++ modules/world.lua
@@ -9,36 +9,27 @@ local world = {
 	HERO_START_POS = vmath.vector3(72, 16, 8),
 	SKULL_START_POS = vmath.vector3(72, 8, 8),
 	CHEST_START_POS = vmath.vector3(72, 288, 0),
-	GAME = { START = 1, PLAY = 2, WIN = 3, LOSE = 4, END = 5 },
-	state = {
-		row = 0,
-		column = 0,
-		length = 0,
-		speed = 0,
-		time = 0,
-		score = 0,
-		game = 0,
-	},
-	entities = {
-		hero = nil,
-		skull = nil,
-		panels = {},
-		rewards = {},
-		torches = "/torches#torches",
-		industry = "/industry#industry"
-	},
-	UI = {
-		hud = "/HUD#hud" 
-	},
-	factories = {
-		hero_factory = "/industry#hero_factory",
-		skull_factory = "/industry#skull_factory",
-		panel_factory = "/industry#panel_factory",
-		block_factory = "/industry#block_factory",
-		coin_factory = "/industry#coin_factory",
-		small_chest_factory = "/industry#small_chest_factory",
-		chest_factory = "/industry#chest_factory",
-		border_factory = "/industry#border_factory"
-	}
+	GAMESTATE = {START = 1, PLAY = 2, WIN = 3, LOSE = 4, END = 5},
+	hero_factory = "/industry#hero_factory",
+	skull_factory = "/industry#skull_factory",
+	panel_factory = "/industry#panel_factory",
+	block_factory = "/industry#block_factory",
+	coin_factory = "/industry#coin_factory",
+	small_chest_factory = "/industry#small_chest_factory",
+	chest_factory = "/industry#chest_factory",
+	border_factory = "/industry#border_factory",
+	gamestate = 0,
+	row = 0,
+	column = 0,
+	length = 0,
+	speed = 0,
+	time = 0,
+	score = 0,
+	torches = "/torches#torches",
+	hud = "/HUD#hud",
+	hero = nil,
+	skull = nil,
+	panels = {},
+	rewards = {}
 }
 return world

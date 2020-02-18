e91c9293af7f21e5247d463e52eb3546083db4b8
y444 <541m0nk1n9@gmail.com>
2020-02-18 20:08:17 +0300

    panels spawning properly

--- game_objects/panel_components/panel.go
+++ game_objects/panel_components/panel.go
@@ -1,27 +1,6 @@
-embedded_components {
-  id: "block_factory"
-  type: "factory"
-  data: "prototype: \"/game_objects/panel_components/block.go\"\n"
-  "load_dynamically: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "border_factory"
-  type: "factory"
-  data: "prototype: \"/game_objects/panel_components/border.go\"\n"
-  "load_dynamically: false\n"
-  ""
+components {
+  id: "panel"
+  component: "/scripts/panel.script"
   position {
     x: 0.0
     y: 0.0
